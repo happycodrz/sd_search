850f1d47e093ae346bb86b866efef3e03773b1d7
Roman Heinrich <roman.heinrich@gmail.com>
2019-01-14 00:31:58 +0100

    mix format

--- lib/request.ex
+++ lib/request.ex
@@ -6,11 +6,12 @@ defmodule SdSearch.Request do
     sort = Keyword.get(opts, :sort, "date")
     page = Keyword.get(opts, :page, 1)
     per_page = Keyword.get(opts, :per_page, 10)
+
     %Request{
       q: URI.encode(q),
       sort: sort,
       num: per_page,
-      start: (page - 1) * per_page,
+      start: (page - 1) * per_page
     }
   end
 end
--- lib/urlbuilder.ex
+++ lib/urlbuilder.ex
@@ -1,6 +1,6 @@
 defmodule SdSearch.UrlBuilder do
   @moduledoc """
-  curl 'https://cse.google.com/cse/element/v1?rsz=filtered_cse&num=10&hl=en&source=gcsc&gss=.com&start=130&cx=010150859881542981030:hqhxyxpwtc4&q=elixir&safe=off&cse_tok=AKaTTZhInrtYdjkwQBS-HG0qKMqK:1547254574756&sort=date&exp=csqr&callback=google.search.cse.api13312&nocache=1547254575205'   --compressed
+  Makes sure we get a valid URL with a fresh token
   """
   def build(req = %SdSearch.Request{}) do
     "https://cse.google.com/cse/element/v1?rsz=filtered_cse&num=#{req.num}&hl=en&source=gcsc&gss=.com&start=#{
@@ -11,7 +11,6 @@ defmodule SdSearch.UrlBuilder do
   end
 
   def cse_token do
-    SdSearch.GetToken.fetch
-    # "AKaTTZja3w-b1myHncaZlfCTZqhI:1547419424060"
+    SdSearch.GetToken.fetch()
   end
 end
